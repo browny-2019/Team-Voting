eb41d9b3b09cedf973c2f73e32a48cc4b8788d5b
browny-2019 <67189412+browny-2019@users.noreply.github.com>
2022-05-30 16:56:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyB1Xy7x-94FUhV6cu7sWWl5XyI7pbRSgRg",
+  authDomain: "team-voting-app-ce60b.firebaseapp.com",
+  databaseURL: "https://team-voting-app-ce60b-default-rtdb.firebaseio.com",
+  projectId: "team-voting-app-ce60b",
+  storageBucket: "team-voting-app-ce60b.appspot.com",
+  messagingSenderId: "394061309349",
+  appId: "1:394061309349:web:5462182243b529939eb44c"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
